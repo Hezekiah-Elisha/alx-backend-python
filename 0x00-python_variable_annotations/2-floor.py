8c6029ba43ab707f98fa2c2e5669d80373401917
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-23 22:17:17 +0300

    :memo: Basic annotations - float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Task 2 module
+Basic annotation - floor
+'''
+
+
+def floor(n: float) -> int:
+    '''A function that returns Math.floor on the argument'''
+    return int(n)
