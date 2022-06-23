1440fc0393229f19c0dfc05d1f27becd978d397e
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-23 22:31:09 +0300

    :memo: Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+task 4 module
+Define and annotate the following variables with the specified values
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
