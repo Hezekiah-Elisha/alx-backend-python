5fbcbf155be69ef8aed701f44f29056cd4e65894
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-23 21:49:33 +0300

    :memo: Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Task 0 Module
->>> type(add(2.1, 4.1))
-<class 'float'>
-"""
+'''Task 0 Module
+'''
 
 
 def add(a: float, b: float) -> float:
-    """ Adds two floats """
+    ''' Adds two floats
+    '''
     return a + b
