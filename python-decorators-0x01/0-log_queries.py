0c40b590b9479141ed5c84acce35b93cff05b9e2
Hezekiah Elisha <elishahezekiah903@gmail.com>
2025-07-18 14:47:49 +0300

    :fire: Enhance logging in SQL query decorator to include timestamp and function name

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,12 +1,14 @@
 import sqlite3
 import functools
+from datetime import datetime
+
 
 #### decorator to lof SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = args[0] if args else kwargs.get("query", "")
-        print(f"Executing query: {query}")
+        print(f"{datetime.now()}: Executing query: {query} with {func.__name__}")
         return func(*args, **kwargs)
 
     return wrapper
